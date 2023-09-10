17f5b2a71b250b411d4dcbf5d7d6868038aca59f
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-09-10 22:24:00 +0100

    finished initial setup

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,56 @@
 #!/usr/bin/env python3
 """Flask engine"""
 
-from flask import Flask, jsonify
+# create a .env file for environment variables
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from os import getenv
+from views import app_views
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+app.register_blueprint(app_views)
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """index route"""
-    return jsonify({'message': 'FitLife360'})
+    return 'FitLife360'
+
+
+@app.errorhandler(401)
+def unauthorized(error):
+    """ 401 Error
+    ---
+    responses:
+      401:
+        description: Unauthorized request
+    """
+    return make_response(jsonify({'error': "Unauthorized"}), 401)
+
+
+@app.errorhandler(403)
+def forbidden(error):
+    """ 403 Error
+    ---
+    responses:
+      403:
+        description: Forbidden request
+    """
+    return make_response(jsonify({'error': "Forbidden"}), 403)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
 
 app.run(
-    host=getenv('HOST') or '0.0.0.0', port=getenv('PORT') or 5000,
-    debug=True, load_dotenv=True
+    host=getenv('HOST', '0.0.0.0'), port=int(getenv('PORT', '5000')),
+    debug=True, load_dotenv=True, threaded=True
 )
---
+++ backend/views/__init__.py
@@ -0,0 +1,2 @@
+from flask import Blueprint
+app_views = Blueprint(name='app_views', import_name=__name__, url_prefix='/api')
