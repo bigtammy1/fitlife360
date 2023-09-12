69f894106d9bdb4e351c643dc31a32646070037f
Pieta <mercy_nwaodu@yahoo.com>
2023-09-12 09:27:03 +0100

    tailwind configuration

---
+++ client/FitLife360/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        'font1': ['Lora', 'sans'],
+        'font2': ['Nunito', 'sans'],
+      },
+      colors: {
+        'primary': '#41827B', 
+        'secondary': '#824148',
+      },
+    },
+  },
+  plugins: [],
+}
+
