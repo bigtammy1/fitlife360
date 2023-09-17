6ffb814c10f0f0d39f48b3b59fa1c6556db0f647
Pieta <mercy_nwaodu@yahoo.com>
2023-09-17 09:45:53 +0100

    trainer dashboard added

--- client/FitLife360/src/App.jsx
+++ client/FitLife360/src/App.jsx
@@ -6,6 +6,9 @@ import Login from './pages/Login';
 import Trainer from './pages/Trainer';
 import Trainee from './pages/Trainee';
 import Register from './pages/Register';
+import Welcome from './pages/trainer/Welcome';
+import Profile from './pages/trainer/Profile';
+import Layout from "./layout/TrainerLayout";
 import NotFound from './pages/NotFound';
 
 function App() {
@@ -18,6 +21,11 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/register/trainee" element={<Trainee />} />
         <Route path="/register/trainer" element={<Trainer />} />
+        <Route path="/dashboard" element={<Layout />}>
+          <Route index element={<Welcome />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
---
+++ client/FitLife360/src/layout/TrainerLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Outlet } from "react-router-dom";
+
+const TrainerLayout = () => {
+  return (
+    <div>TrainerLayout
+        <Outlet />
+    </div>
+  )
+}
+
+export default TrainerLayout
